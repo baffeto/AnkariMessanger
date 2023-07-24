6695e62955ee3a97749c6b5745dd1f454645206b
baffeto <baffeto@mail.ru>
2023-07-24 13:00:03 +0300

    Create a base.html

--- ankari/users/views.py
+++ ankari/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+
+class SignUp(View):
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+            
+            return redirect()
+        
+        return render('users/main.html', {
+            'form': form,
+        })
+            
+
+
