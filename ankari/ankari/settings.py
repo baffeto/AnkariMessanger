3caee5dbc15e21169e84fdb049de4053fcc767e9
baffeto <baffeto@mail.ru>
2023-07-28 00:20:23 +0300

    Updated settings.py

--- ankari/ankari/settings.py
+++ ankari/ankari/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'ankari.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ankari')
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- ankari/profiles/models.py
+++ ankari/profiles/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="Nothing", max_length=300)
+    email = models.EmailField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    avatar = models.ImageField(default='avatar.png', upload_to='avatars/')
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+    slug = models.SlugField(unique=True, blank=True)
+    # updated 
+    # created
