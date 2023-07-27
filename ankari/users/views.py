decd77e98bbc3812c39b9100467c86a25a6ace7e
baffeto <baffeto@mail.ru>
2023-07-28 00:01:28 +0300

    Deleted files + v0.0.1

--- ankari/users/views.py
+++ ankari/users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import SignUpForm
-from .models import Profile``
 
 
 def main_page(request):
@@ -22,14 +21,3 @@ def signup(request):
         form = SignUpForm()
         
     return render(request, 'users/signup.html', {'form': form})
-
-
-def my_profile(request):
-    template_name = 'users/profile.html'
-    profile = Profile.objects.get(user=request.user)
-    
-    context = {
-        'profile': profile
-    }
-    
-    return render(request, template_name, context)
