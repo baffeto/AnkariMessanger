1611727a40f21528803871a04420c66acb1cdb96
baffeto <baffeto@mail.ru>
2023-07-27 14:39:08 +0300

    Create a Profile model

--- ankari/users/models.py
+++ ankari/users/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    friends = models.ManyToManyField(User, related_name='friends', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return str(self.user)
