77b0b462e20908993fda368cfff9f473f729187d
baffeto <baffeto@mail.ru>
2023-07-27 16:36:17 +0300

    Updated urls.py

--- ankari/users/urls.py
+++ ankari/users/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('myprofile/', views.my_profile, name='my-profile-view'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
