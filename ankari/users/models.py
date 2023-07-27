fb33e25dfe6080d675aba07b5c4f742839889277
baffeto <baffeto@mail.ru>
2023-07-27 16:22:01 +0300

    Modified models.py - friends

--- ankari/users/models.py
+++ ankari/users/models.py
@@ -9,5 +9,26 @@ class Profile(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    def get_friends(self):
+        return self.friends.all()
+    
+    def get_friends_quantity(self):
+        return self.friends.all().count()
+    
     def __str__(self) -> str:
         return str(self.user)
+    
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted'),
+)
+    
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"{self.sender} - {self.receiver} - {self.status}"
