281a079ba128b69d8b910ce5e4582d0f1e6db6a5
baffeto <baffeto@mail.ru>
2023-07-29 11:45:38 +0300

    Create form of my profile and updated settings to fix error staticfiles_dirs

--- ankari/ankari/settings.py
+++ ankari/ankari/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ankari')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ankari", "static_cdn", "static_root")
--- ankari/profiles/views.py
+++ ankari/profiles/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileModelForm
 
 
 def profile_view(request):
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm()
     
     context = {
         'profile': profile,
+        'form': form,
     }
     
     return render(request, 'profiles/profile.html', context)
